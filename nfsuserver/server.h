af8791a27985de6c232447b68520c2d401f81ac1
ian-travers <mr.itpe@gmail.com>
2022-05-10 11:00:11 +0300

    Delete unused defs

--- nfsuserver/server.h
+++ nfsuserver/server.h
@@ -2,19 +2,17 @@
 struct RegUser {
 	char Username[100];
 	char Personas[4][100];
-	RegUser * Next;
+	RegUser* Next;
 };
 
 class RegUsers {
-	public:
-		int Count;
-		RegUser * First;
-		RegUser * Last;
-		void Load ();
-		void Save ();
-		RegUser * UserFromUsername (char * username);
-		void AddUser (RegUser * user);
-	void RemoveUser (RegUser * user);
+public:
+	int Count;
+	RegUser* First;
+	RegUser* Last;
+	RegUser* UserFromUsername(char* username);
+	void AddUser(RegUser* user);
+	void RemoveUser(RegUser* user);
 };
 
 class RoomClass;
@@ -22,107 +20,103 @@ class GameClass;
 class ConnectionClass;
 
 class UserClass {
-	public:
-		int Version;
-		int id;
-		int Rank[4];
-		GameClass * Game;
-		ConnectionClass * Connection;
-		char Username[100];
-		char Personas[4][100];
-		char car[1024];
-		char IP[20];
-		char Port[10];
-		UserClass ();
-		void SelectPerson (char * person);
-		int SelectedPerson;
-		UserClass * Next;
-		RoomClass * CurrentRoom;
+public:
+	int Version;
+	int id;
+	int Rank[4];
+	GameClass* Game;
+	ConnectionClass* Connection;
+	char Username[100];
+	char Personas[4][100];
+	char car[1024];
+	char IP[20];
+	char Port[10];
+	UserClass();
+	void SelectPerson(char* person);
+	int SelectedPerson;
+	UserClass* Next;
+	RoomClass* CurrentRoom;
 	int Idle;
 };
 
 class UsersClass {
-	public:
-		int cid;
-		unsigned int Count;
-		UserClass * First;
-		void AddUser (UserClass * user);
-		void RemoveUser (UserClass * user);
-		void Broadcast (char *buf, unsigned int len);
-		UserClass * UserFromUsername (char * username);
-	UsersClass ();
+public:
+	int cid;
+	unsigned int Count;
+	UserClass* First;
+	void AddUser(UserClass* user);
+	void RemoveUser(UserClass* user);
+	void Broadcast(char* buf, unsigned int len);
+	UserClass* UserFromUsername(char* username);
+	UsersClass();
 };
 
 class RUserClass {
-	public:
-		UserClass * User;
-	RUserClass * Next;
+public:
+	UserClass* User;
+	RUserClass* Next;
 };
 
 class RUsersClass {
-	public:
-		unsigned int Count;
-		RUserClass * First;
-		void AddUser (RUserClass * user);
-		void RemoveUser (RUserClass * user);
-		void Broadcast (char *buf, unsigned int len);
-		RUserClass * UserFromUsername (char * username);
-	RUsersClass ();
+public:
+	unsigned int Count;
+	RUserClass* First;
+	void AddUser(RUserClass* user);
+	void RemoveUser(RUserClass* user);
+	RUsersClass();
 };
 
 class GameClass {
-	public:
-		bool working;
-		int max, min;
-		int Count;
-		int ID;
-		int sysflags;
-		char params[300];
-		char Name[200];
-		RUsersClass Users;
-		void AddUser (UserClass * user, char * buffer);
-		void RemoveUser (UserClass * user, char * buffer);
-		void Refresh ();
-		void SendInfoToUser (UserClass * user, char * buffer);
-		void StartGame (char * buffer);
-	GameClass * Next;
+public:
+	bool working;
+	int max, min;
+	int Count;
+	int ID;
+	int sysflags;
+	char params[300];
+	char Name[200];
+	RUsersClass Users;
+	void AddUser(UserClass* user, char* buffer);
+	void RemoveUser(UserClass* user, char* buffer);
+	void SendInfoToUser(UserClass* user, char* buffer);
+	void StartGame(char* buffer);
+	GameClass* Next;
 };
 
 class GamesClass {
-	public:
-		int cid;
-		GameClass * First;
-		int Count;
-		void AddGame (GameClass * game);
-		void RemoveGame (GameClass * game);
-		GameClass * GameFromID (int id);
-	GameClass * GameFromName (char * name);
+public:
+	int cid;
+	GameClass* First;
+	int Count;
+	void AddGame(GameClass* game);
+	void RemoveGame(GameClass* game);
+	GameClass* GameFromName(char* name);
 };
 
 class RoomClass {
-	public:
-		RUsersClass Users;
-		GamesClass Games;
-		int Count;
-		int ID;
-		bool IsGlobal;
-		char Name[100];
-		RoomClass * Next;
-		void AddUser (UserClass * user, char * buffer);
-		void RemoveUser (UserClass * user, char * buffer);
-		void ListToUser (UserClass * user, char * buffer);
-		void RefreshUser (UserClass * user, char * buffer);
+public:
+	RUsersClass Users;
+	GamesClass Games;
+	int Count;
+	int ID;
+	bool IsGlobal;
+	char Name[100];
+	RoomClass* Next;
+	void AddUser(UserClass* user, char* buffer);
+	void RemoveUser(UserClass* user, char* buffer);
+	void ListToUser(UserClass* user, char* buffer);
+	void RefreshUser(UserClass* user, char* buffer);
 };
 
 class RoomsClass {
-	public:
-		int cid;
-		int Count;
-		RoomClass * First;
-		void AddRoom (RoomClass * room);
-		void RemoveRoom (RoomClass * room);
-		RoomClass * RoomFromName (char * name);
-	RoomsClass ();
+public:
+	int cid;
+	int Count;
+	RoomClass* First;
+	void AddRoom(RoomClass* room);
+	void RemoveRoom(RoomClass* room);
+	RoomClass* RoomFromName(char* name);
+	RoomsClass();
 };
 /*
 class SessionClass{
@@ -145,27 +139,27 @@ public:
 };
 */
 class ServerClass {
-	public:
-		//SessionsClass Sessions;
-		char Name[100];
-		char ServerIP[100];
-		char ServerExternalIP[100];
-		char WelcomeMessage[100];
-		time_t Startup;
-		UsersClass Users;
-		RoomsClass Rooms;
-		RegUsers ru;
-		void SendRoomsToUser (UserClass * user, char * buffer);
-		ServerClass ();
-		void LoadSettings ();
-		void SaveSettings ();
-	bool RegisterUser (char * username);
+public:
+	//SessionsClass Sessions;
+	char Name[100];
+	char ServerIP[100];
+	char ServerExternalIP[100];
+	char WelcomeMessage[100];
+	time_t Startup;
+	UsersClass Users;
+	RoomsClass Rooms;
+	RegUsers ru;
+	void SendRoomsToUser(UserClass* user, char* buffer);
+	ServerClass();
+	void LoadSettings();
+	void SaveSettings();
+	bool RegisterUser(char* username);
 };
 
-bool RecvCommand( SOCKET sock, char *buffer );
-bool SendCommand( SOCKET sock, char *command, char *params [], int paramcount );
-int MakeCommand( char *buffer, char *command, char *params [], int paramcount );
-void BroadCastCommand( UsersClass *users, char *command, char *params [], int paramcount, char *buffer );
-void BroadCastCommand( RUsersClass&users, char *command, char *params [], int paramcount, char *buffer );
-int recvn( SOCKET sock, char *buf, int required );
-int atoi2( char *str );
+bool RecvCommand(SOCKET sock, char* buffer);
+bool SendCommand(SOCKET sock, char* command, char* params[], int paramcount);
+int MakeCommand(char* buffer, char* command, char* params[], int paramcount);
+void BroadCastCommand(UsersClass* users, char* command, char* params[], int paramcount, char* buffer);
+void BroadCastCommand(RUsersClass& users, char* command, char* params[], int paramcount, char* buffer);
+int recvn(SOCKET sock, char* buf, int required);
+int atoi2(char* str);
--- nfsuserver/win_nix.h
+++ nfsuserver/win_nix.h
@@ -1,69 +1,68 @@
 #ifndef _WIN_NIX_H_
-	#define _WIN_NIX_H_
+#define _WIN_NIX_H_
 
-	//define NT_SERVICE to enable service mode
-	//#define NT_SERVICE
+//define NT_SERVICE to enable service mode
+//#define NT_SERVICE
 
-	#include <time.h>
-	#include <stdio.h>
-	#include <math.h>
+#include <time.h>
+#include <stdio.h>
+#include <math.h>
 
-	#ifdef _WIN32
-		#include <Ws2tcpip.h>
-		#ifdef NT_SERVICE
-			#include <windows.h>
-			#include <Winsvc.h>
-		#endif
-		#include <process.h>
-		#include <winsock.h>
-	#else
-		#undef NT_SERVICE
-		#include <signal.h>
-		#include <errno.h>
-		#include <fcntl.h>
-		#include <string.h>
-		#include <unistd.h>
-		#include <sys/ioctl.h>
-		#include <sys/types.h>
-		#include <sys/termios.h>
-		#include <sys/socket.h>
-		#include <netinet/in.h>
-		#include <malloc.h>
-		#include <pthread.h>
-		#include <stdlib.h>
-		#include <ctype.h>
-		#include <arpa/inet.h>
-		#include <netdb.h>
-		#include <errno.h>
-	#endif
-	#ifdef _WIN32
-		#define RETURNFROMTHREAD return
-	#else
-		#define RETURNFROMTHREAD return NULL
-		#define SOCKADDR sockaddr
-		#define SOCKADDR_IN sockaddr_in
-		#define _strdup strdup
-		#define SOCKET int
-		#define INVALID_SOCKET (SOCKET)(~0)
-		#define HANDLE pthread_t
-		#define BOOL bool
-		#define DWORD long
-		#define TRUE 1
-		#define FALSE 0
-		#define SOCKET_ERROR -1
-		#define Sleep(x) usleep(x*1000)
-		#define closesocket close
-		#define RETURNFROMTHREAD return NULL
-		#define stricmp   strcasecmp
-		#define _beginthread(func, stack, data) pthread_create(&glob_hack_tid, NULL, (void*(*)(void*))func, data)
-		static pthread_t glob_hack_tid;
-	#endif
-	#ifdef _WIN32
-		#define threadfunc void
-		#define SERVER_PLATFORM "win32"
-	#else
-		#define threadfunc void *
-		#define SERVER_PLATFORM "*nix"
-	#endif
+#ifdef _WIN32
+#include <Ws2tcpip.h>
+#ifdef NT_SERVICE
+#include <windows.h>
+#include <Winsvc.h>
+#endif
+#include <process.h>
+#include <winsock.h>
+#else
+#undef NT_SERVICE
+#include <signal.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/termios.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <malloc.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <errno.h>
+#endif
+#ifdef _WIN32
+#define RETURNFROMTHREAD return
+#else
+#define RETURNFROMTHREAD return NULL
+#define SOCKADDR sockaddr
+#define SOCKADDR_IN sockaddr_in
+#define _strdup strdup
+#define SOCKET int
+#define INVALID_SOCKET (SOCKET)(~0)
+#define HANDLE pthread_t
+#define BOOL bool
+#define DWORD long
+#define TRUE 1
+#define FALSE 0
+#define SOCKET_ERROR -1
+#define Sleep(x) usleep(x*1000)
+#define closesocket close
+#define stricmp strcasecmp
+#define _beginthread(func, stack, data) pthread_create(&glob_hack_tid, NULL, (void*(*)(void*))func, data)
+static pthread_t glob_hack_tid;
+#endif
+#ifdef _WIN32
+#define threadfunc void
+#define SERVER_PLATFORM "win32"
+#else
+#define threadfunc void *
+#define SERVER_PLATFORM "*nix"
+#endif
 
 #endif //_WIN_NIX_H_
